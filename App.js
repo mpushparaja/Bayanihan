0382ffaed3c543f61b55fd359896c92e293ea17e
Pushparaja M <116150130+mpushparaja@users.noreply.github.com>
2023-10-04 03:02:23 +0530

    splash screen and icon update

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from "./Routes";
 import {Provider} from './Context';
+import SplashScreen from 'react-native-splash-screen';
+import { Platform } from 'react-native';
 
 const App = () => {
+
+  useEffect(()=>{
+    if(Platform.OS === 'android')
+    SplashScreen.hide();
+  },[]);
+
   return (
     <Provider>
       <Routes />
